66c7c0ef0a9bd29a5f78aeff797bf541d0b91624
Pablo Alessandro Santos Hugen <phugen@redhat.com>
2026-03-04 11:02:05 -0300

    update
    Signed-off-by: Pablo Alessandro Santos Hugen <phugen@redhat.com>

--- build.zig
+++ build.zig
@@ -1,33 +1,55 @@
 const std = @import("std");
+const LinkMode = std.builtin.LinkMode;
 
-pub fn build(b: *std.Build) void {
+const manifest = @import("build.zig.zon");
+
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const linkage = b.option(std.builtin.LinkMode, "linkage", "Library linkage type") orelse .static;
-
-    const upstream = b.dependency("upstream", .{});
-    const src = upstream.path("");
-
     const os = target.result.os.tag;
     const arch = target.result.cpu.arch;
 
-    const mod = b.createModule(.{ .target = target, .optimize = optimize, .link_libc = true });
-    mod.addIncludePath(src.path(b, "include"));
-    mod.addCSourceFiles(.{ .root = src, .files = sources, .flags = &((if (os != .windows) .{
-        "-fvisibility=hidden",
-        "-DHAVE_FUNC_ATTRIBUTE_VISIBILITY=1",
-        "-DHAVE_GMTIME_R=1",
-    } else .{ "", "", "" }) ++
-        .{ "-DHasTHREADS=1", "-DHAVE_TIMESPEC_GET=1" } ++
-        .{if (arch.endian() == .big) "-DWORDS_BIGENDIAN=1" else ""} ++
-        .{if (arch != .x86_64 and arch != .x86) "-DCMS_DONT_USE_SSE2=1" else ""}) });
+    const options = .{
+        .linkage = b.option(LinkMode, "linkage", "Library linkage type") orelse
+            .static,
+    };
 
-    const lib = b.addLibrary(.{ .name = "lcms2", .root_module = mod, .linkage = linkage });
-    inline for (.{ "lcms2.h", "lcms2_plugin.h" }) |h| lib.installHeader(src.path(b, "include/" ++ h), h);
+    const upstream = b.dependency("lcms2_c", .{});
+
+    const mod = b.createModule(.{
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+
+    mod.addIncludePath(upstream.path("include"));
+
+    mod.addCMacro("HasTHREADS", "1");
+    mod.addCMacro("HAVE_TIMESPEC_GET", "1");
+    if (os != .windows) {
+        mod.addCMacro("HAVE_FUNC_ATTRIBUTE_VISIBILITY", "1");
+        mod.addCMacro("HAVE_GMTIME_R", "1");
+    }
+    if (arch.endian() == .big) mod.addCMacro("WORDS_BIGENDIAN", "1");
+    if (arch != .x86_64 and arch != .x86) mod.addCMacro("CMS_DONT_USE_SSE2", "1");
+
+    mod.addCSourceFiles(.{
+        .root = upstream.path(""),
+        .files = src,
+        .flags = if (os != .windows) &.{"-fvisibility=hidden"} else &.{},
+    });
+
+    const lib = b.addLibrary(.{
+        .name = "lcms2",
+        .root_module = mod,
+        .linkage = options.linkage,
+        .version = try .parse(manifest.version),
+    });
+    inline for (.{ "lcms2.h", "lcms2_plugin.h" }) |h| lib.installHeader(upstream.path("include/" ++ h), h);
     b.installArtifact(lib);
 }
 
-const sources: []const []const u8 = &.{
+const src: []const []const u8 = &.{
     "src/cmsalpha.c", "src/cmscam02.c", "src/cmscgats.c",  "src/cmscnvrt.c",
     "src/cmserr.c",   "src/cmsgamma.c", "src/cmsgmt.c",    "src/cmshalf.c",
     "src/cmsintrp.c", "src/cmsio0.c",   "src/cmsio1.c",    "src/cmslut.c",
